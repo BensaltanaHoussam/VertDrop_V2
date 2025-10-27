e2611cfdd8bc99deb58c1dfce4f178fdccd3876b
BensaltanaHoussam <bensaltna07@gmail.com>
2025-10-27 11:48:56 +0100

    Add initial project structure with Maven configuration and database schema

---
+++ src/main/java/com/vertdrop_v2/VertDropV2Application.java
@@ -0,0 +1,13 @@
+package com.vertdrop_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertDropV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VertDropV2Application.class, args);
+    }
+
+}
