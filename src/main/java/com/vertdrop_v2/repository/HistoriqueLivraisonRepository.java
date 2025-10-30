d95192cae67f3f7f08c399a42723736cc6a503a4
BensaltanaHoussam <bensaltna07@gmail.com>
2025-10-30 16:32:41 +0100

    Add method to retrieve delivery history for a colis

--- src/main/java/com/vertdrop_v2/repository/HistoriqueLivraisonRepository.java
+++ src/main/java/com/vertdrop_v2/repository/HistoriqueLivraisonRepository.java
@@ -4,7 +4,11 @@ import com.vertdrop_v2.entity.HistoriqueLivraison;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface HistoriqueLivraisonRepository extends JpaRepository<HistoriqueLivraison, Long> {
+
+    List<HistoriqueLivraison> findByColisIdOrderByDateChangementDesc(Long colisId);
 }
